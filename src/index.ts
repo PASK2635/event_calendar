792c19b1a2c7a56cb3525b1330b797c54747152d
Markus Kristian Simonsen <markuskristiansimonsen@gmail.com>
2023-09-08 22:55:32 +0200

    change string to datetime

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,22 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const app = new Elysia()
+  .model({
+    event: t.Object({
+      name: t.String(),
+      description: t.String(),
+      start: t.Date(),
+      end: t.Union([t.Null(), t.Date()]),
+      rsvp: t.Union([t.Null(), t.Date()]),
+      ownerId: t.String(),
+    }),
+    user: t.Object({ id: t.String(), name: t.String() }),
+  })
   .post(
     "/users",
     async ({ body }) =>
       await prisma.user.create({ data: { id: body.id, name: body.name } }),
-    { body: t.Object({ id: t.String(), name: t.String() }) }
+    { body: "user" }
   )
   .get(
     "/events/:id",
@@ -22,15 +33,9 @@ const app = new Elysia()
       },
     }
   )
+  .get("/events", () => prisma.event.findMany())
   .post("/events", ({ body }) => prisma.event.create({ data: body }), {
-    body: t.Object({
-      name: t.String(),
-      description: t.String(),
-      start: t.String(),
-      end: t.String(),
-      rsvp: t.String(),
-      ownerId: t.String(),
-    }),
+    body: "event",
   })
   .put(
     "/events/:id",
@@ -38,14 +43,7 @@ const app = new Elysia()
       prisma.event.update({ where: { id: id }, data: body }),
     {
       params: t.Object({ id: t.Number() }),
-      body: t.Object({
-        name: t.String(),
-        description: t.String(),
-        start: t.String(),
-        end: t.String(),
-        rsvp: t.String(),
-        ownerId: t.String(),
-      }),
+      body: "event",
       transform({ params }) {
         const id = parseInt(params.id + "");
         if (!Number.isNaN(id)) params.id = id;
@@ -92,18 +90,6 @@ const app = new Elysia()
     }
   )
   .listen(3000);
-
-/*
-id    Int     @id @default(autoincrement())
-  name String  @unique
-  description  String?
-  start String
-  end String?
-  rsvp String?
-  participants EventParticipants[]
-  owner User @relation(fields: [ownerId], references: [id])
-  ownerId String
-*/
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
