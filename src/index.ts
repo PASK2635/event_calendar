e475c46a1c4c0877afd7e547e19cb3175048fd35
Markus Kristian Simonsen <markuskristiansimonsen@gmail.com>
2023-09-08 22:00:05 +0200

    able to create events

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,35 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const app = new Elysia()
-  .post("/users", ({ body }) =>
-    prisma.user.create({ data: { id: body.id, name: body.name } })
+  .post(
+    "/users",
+    async ({ body }) =>
+      await prisma.user.create({ data: { id: body.id, name: body.name } }),
+    { body: t.Object({ id: t.String(), name: t.String() }) }
   )
+  .post("/events", ({ body }) => prisma.event.create({ data: body }), {
+    body: t.Object({
+      name: t.String(),
+      description: t.String(),
+      start: t.String(),
+      end: t.String(),
+      rsvp: t.String(),
+      ownerId: t.String(),
+    }),
+  })
   .listen(3000);
+
+/*
+id    Int     @id @default(autoincrement())
+  name String  @unique
+  description  String?
+  start String
+  end String?
+  rsvp String?
+  participants EventParticipants[]
+  owner User @relation(fields: [ownerId], references: [id])
+  ownerId String
+*/
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
